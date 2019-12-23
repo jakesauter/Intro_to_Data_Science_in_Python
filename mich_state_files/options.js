43b40103d82d2b50be0e9382f6bb0e94502bc967
Jake Sauter <jsauter@oswego.edu>
2019-12-23 14:58:29 -0500

    removed website files

--- mich_state_files/options.js
+++
@@ -1,9 +0,0 @@
-﻿define(['module'], function (module) {
-    var Options = function () {
-        this.sport_id = module.config().sport_id;
-		this.dummy_ads = module.config().dummy_ads;
-		this.all_ads = module.config().all_ads;
-    };
-
-    return Options;
-});
